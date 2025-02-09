0b21d715d7341a4d153e462281dbec09d0b400e6
Alexander Khrichikov <alex-ander-97@mail.ru>
2025-02-10 00:25:06 +0500

    Опепил

--- 3/3.11/3.11.11-Order_2.py
+++ 3/3.11/3.11.11-Order_2.py
@@ -2,26 +2,26 @@ class User:
     def __init__(self, login, balance=0):
         self.login = login
         self.balance = balance
-        
+
     @property
     def balance(self):
         return self.__balance
-    
+
     @balance.setter
     def balance(self, value):
         self.__balance = value
-        
+
     def deposit(self, value):
         self.balance += value
-        
+
     def is_money_enough(self, value):
         return self.balance >= value
-    
+
     def payment(self, value):
         if not self.is_money_enough(value):
             print("Не хватает средств на балансе. Пополните счет")
             return None
         self.balance -= value
-                    
+
     def __str__(self):
         return f'Пользователь {self.login}, баланс - {self.balance}'
