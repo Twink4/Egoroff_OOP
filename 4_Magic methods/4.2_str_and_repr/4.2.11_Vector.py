4759b5b9036fc86c129add580c56d9628b0b1751
Alexander Khrichikov <alex-ander-97@mail.ru>
2025-02-11 22:53:51 +0500

    Nothing

--- 4_Magic methods/4.2_str_and_repr/4.2.11_Vector.py
+++ 4_Magic methods/4.2_str_and_repr/4.2.11_Vector.py
@@ -1,9 +1,8 @@
 class Vector:
     def __init__(self, *args):
         self.nums_list = sorted(el for el in args if type(el) is int)
-        
-        
+
     def __str__(self):
         return f"Вектор{tuple(self.nums_list)}"\
-        if len(self.nums_list)\
-        else "Пустой вектор"
+            if len(self.nums_list)\
+            else "Пустой вектор"
