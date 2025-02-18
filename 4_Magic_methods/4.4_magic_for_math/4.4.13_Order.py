dd6f2ee099b95236649d241b34489a1d0b2a14e5
Alexander <alex-ander-97@mail.ru>
2025-02-18 21:13:47 +0500

    4.4.13 Completed

--- 4_Magic_methods/4.4_magic_for_math/4.4.13_Order.py
+++ 4_Magic_methods/4.4_magic_for_math/4.4.13_Order.py
@@ -6,8 +6,16 @@ class Order:
     def __add__(self, product):
         return Order(self.cart + [product], self.customer)
 
-    def __radd__(self):
-        ...
+    def __radd__(self, product):
+        return Order([product] + self.cart, self.customer)
+
+    def __sub__(self, product):
+        if product in self.cart:
+            self.cart.remove(product)
+        return Order(self.cart, self.customer)
+
+    def __rsub__(self, product):
+        return self.__sub__(product)
 
 
 order = Order(['banana', 'apple'], 'Гена Букин')
@@ -16,3 +24,20 @@ order_2 = order + 'orange'
 assert order.cart == ['banana', 'apple']
 assert order.customer == 'Гена Букин'
 assert order_2.cart == ['banana', 'apple', 'orange']
+
+order = 'mango' + order
+assert order.cart == ['mango', 'banana', 'apple']
+order = 'ice cream' + order
+assert order.cart == ['ice cream', 'mango', 'banana', 'apple']
+
+order = order - 'banana'
+assert order.cart == ['ice cream', 'mango', 'apple']
+
+order3 = order - 'banana'
+assert order3.cart == ['ice cream', 'mango', 'apple']
+
+order = order - 'mango'
+assert order.cart == ['ice cream', 'apple']
+order = 'lime' - order
+assert order.cart == ['ice cream', 'apple']
+print('Good')
