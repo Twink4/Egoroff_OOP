fa9b9ff376556ce7fb1bbe6eeaaaebc1b94bd77e
Alexander <alex-ander-97@mail.ru>
2025-03-31 15:08:01 +0500

    4.9.22 Completed

---
+++ 4_Magic_methods/4.9_getitem_setitem_delitem/4.9.22_Playlist.py
@@ -0,0 +1,59 @@
+class Song:
+    def __init__(
+        self,
+        title: str,
+        artist: str
+    ):
+        self.title = title
+        self.artist = artist
+    
+class Playlist:
+    def __init__(
+        self,        
+    ):
+        self.songs: list = []
+        
+    def __getitem__(
+        self,
+        index
+    ):
+        return self.songs[index]
+    
+    def __setitem__(
+        self,
+        index,
+        song
+    ):
+        self.songs.insert(index, song)
+        
+    def add_song(
+        self,
+        song
+    ):
+        self.songs.append(song)
+        
+        
+        
+#TESTS
+
+playlist = Playlist()
+assert len(playlist.songs) == 0
+assert isinstance(playlist, Playlist)
+playlist.add_song(Song("Paradise", "Coldplay"))
+assert playlist[0].title == 'Paradise'
+assert playlist[0].artist == 'Coldplay'
+assert len(playlist.songs) == 1
+
+playlist[0] = Song("Resistance", "Muse")
+assert playlist[0].title == 'Resistance'
+assert playlist[0].artist == 'Muse'
+assert playlist[1].title == 'Paradise'
+assert playlist[1].artist == 'Coldplay'
+
+playlist[1] = Song("Helena", "My Chemical Romance")
+assert playlist[1].title == 'Helena'
+assert playlist[1].artist == 'My Chemical Romance'
+
+assert playlist[2].title == 'Paradise'
+assert playlist[2].artist == 'Coldplay'
+print('Good')
