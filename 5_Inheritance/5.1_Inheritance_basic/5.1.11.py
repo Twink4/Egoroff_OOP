e207a85b4c106db9e5ab14fd8392410110340c92
Twink4 <alex-ander-97@mail.ru>
2025-04-01 14:16:01 +0500

    5.1.11 Compleed

--- 5_Inheritance/5.1_Inheritance_basic/5.1.11.py
+++ 5_Inheritance/5.1_Inheritance_basic/5.1.11.py
@@ -1,24 +1,23 @@
 class Vehicle:
     ...
-    
-    
+
+
 class Car(Vehicle):
     ...
-    
-    
+
+
 class RaceCar(Car):
     ...
-    
-    
-    
+
+
 class Plane(Vehicle):
     ...
-    
-    
+
+
 class Boat(Vehicle):
     ...
-    
-    
+
+
 vehicle = Vehicle()
 car = Car()
 plane = Plane()
@@ -34,8 +33,11 @@ assert vehicle.__dict__ == {}
 assert car.__dict__ == {}
 
 assert issubclass(Car, Vehicle), "Класс Car должен наследоваться от Venicle"
-assert issubclass(Plane, Vehicle), "Класс Plane должен наследоваться от Venicle"
+assert issubclass(
+    Plane, Vehicle), "Класс Plane должен наследоваться от Venicle"
 assert issubclass(Boat, Vehicle), "Класс Boat должен наследоваться от Venicle"
-assert issubclass(RaceCar, Car), "Класс RaceCar должен наследоваться от Venicle"
-assert issubclass(RaceCar, Vehicle), "Класс RaceCar должен наследоваться от Venicle"
+assert issubclass(
+    RaceCar, Car), "Класс RaceCar должен наследоваться от Venicle"
+assert issubclass(
+    RaceCar, Vehicle), "Класс RaceCar должен наследоваться от Venicle"
 print('Good')
